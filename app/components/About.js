cd6d3095e966b6cbc3ff3feefd712fccbf670874
Gordon Mohrin <g.mohrin@naisone.de>
2017-03-25 20:56:10 +0100

    feat(about) add about section

---
+++ app/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="page-header">About</h1>
+                <img src="./assets/people.png/" />
+                <br />
+                Team Plesk:
+                <br />
+                Team Freelance:
+            </div>
+        );
+    }
+}
--- app/components/App.js
+++ app/components/App.js
@@ -4,6 +4,7 @@ import Dashboard from './Dashboard';
 import ServerDetails from './ServerDetails';
 import ServerList from './ServerList';
 import ConnectForm from './ConnectForm';
+import About from "./About";
 
 export default class App extends React.Component {
   render() {
@@ -15,13 +16,17 @@ export default class App extends React.Component {
               <ul className="nav nav-sidebar">
                 <li><Link to="/">Dashboard</Link></li>
               </ul>
-              <h5>Servers <Link to="/server/add"><span className="glyphicon glyphicon-plus"></span></Link></h5>
+              <h5>Servers <Link to="/server/add"><button className="btn btn-sm glyphicon glyphicon-plus"/></Link></h5>
               <ServerList/>
+              <Link to="/about" className="fixed-bottom"><button className="btn btn-sm">About</button></Link>
             </div>
             <div className="col-xs-9 col-xs-offset-3 col-xs-10 col-xs-offset-2 main">
               <Route exact path="/" component={Dashboard}/>
               <Route exact path="/server/add" component={ConnectForm}/>
               <Route path="/server/show/:serverName" component={ServerDetails}/>
+              <Route path="/about" component={About}/>
+            </div>
+            <div>
             </div>
           </div>
         </div>
