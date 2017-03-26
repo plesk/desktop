e97e9b95dea3b0c9fc0f78e04fae455fe73ba95f
Eugene Kazakov <ekazakov@plesk.com>
2017-03-26 15:47:44 +0700

    Sidebar should be always visible

--- app/components/App.js
+++ app/components/App.js
@@ -11,14 +11,14 @@ export default class App extends React.Component {
       <Router>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-sm-3 col-md-2 sidebar">
+            <div className="col-sm-2 col-xs-3 sidebar">
               <ul className="nav nav-sidebar">
                 <li><Link to="/">Dashboard</Link></li>
               </ul>
               <h5>Servers <Link to="/server/add"><span className="fa fa-plus"/></Link></h5>
               <ServerList/>
             </div>
-            <div className="col-xs-9 col-xs-offset-3 col-xs-10 col-xs-offset-2 main">
+            <div className="col-sm-10 col-sm-offset-2 col-xs-9 col-xs-offset-3 main">
               <Route exact path="/" component={Dashboard}/>
               <Route path="/server/add" component={ConnectForm}/>
               <Route path="/server/show/:serverName" component={ServerDetails}/>
