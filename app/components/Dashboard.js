f01a29c92049f38c4d3d51f104a566d666ae9945
froth <frank.roth@krauth-online.de>
2017-03-26 11:55:35 +0200

    Tabs replaced with whitespaces

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -3,35 +3,35 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom';
 const storage = window.require('electron-json-storage');
 
 export default class Dashboard extends React.Component {
-	componentDidMount() {
-		storage.get('servers', (error, servers) => {
-			this.setState({'servers': servers});
-		});
-	}
+  componentDidMount() {
+    storage.get('servers', (error, servers) => {
+      this.setState({'servers': servers});
+    });
+  }
 
-	serverCount() {
-		if (this.state && this.state.servers) {
-			return Object.keys(this.state.servers).length || 0;
-		}
-		return 0;
-	}
+  serverCount() {
+    if (this.state && this.state.servers) {
+      return Object.keys(this.state.servers).length || 0;
+    }
+    return 0;
+  }
 
-	render() {
-		return (
-			<div>
-				<h1 className="page-header">Dashboard</h1>
-				<p>
-					<Link to="/server/add" className="btn btn-default">Connect a server</Link>
-				</p>
-				<table className="table table-bordered table-hover">
-					<tbody>
-					<tr>
-						<th>Servers</th>
-						<td>{this.serverCount()}</td>
-					</tr>
-					</tbody>
-				</table>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h1 className="page-header">Dashboard</h1>
+        <p>
+          <Link to="/server/add" className="btn btn-default">Connect a server</Link>
+        </p>
+        <table className="table table-bordered table-hover">
+          <tbody>
+          <tr>
+            <th>Servers</th>
+            <td>{this.serverCount()}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
