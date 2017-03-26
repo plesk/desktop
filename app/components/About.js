7872829c6fb5668535a3b2b7b099afb4307858a0
Alexey Sinko <alexeysinko@gmail.com>
2017-03-26 20:26:17 +0700

    Add reusable ExternalLink component

--- app/components/About.js
+++ app/components/About.js
@@ -1,24 +1,15 @@
 import React from 'react';
-const shell = window.require('electron').shell;
+import ExternalLink from './ExternalLink';
 
 export default class About extends React.Component {
-
-  open(event) {
-    shell.openExternal(event.target.href);
-    event.preventDefault();
-  }
-
   render() {
     return (
       <div>
         <h1 className="page-header">About</h1>
         <p>
-          GitHub: <a onClick={this.open} href="https://github.com/plesk/desktop">https://github.com/plesk/desktop</a>
-          <br/>
-          Wiki: <a onClick={this.open} href="https://github.com/plesk/desktop/wiki">https://github.com/plesk/desktop/wiki</a>
-          <br/>
-          Ticket: <a onClick={this.open} href="https://github.com/plesk/desktop/issues/new">https://github.com/plesk/desktop/issues/new</a>
-          <br/>
+          GitHub: <ExternalLink href="https://github.com/plesk/desktop">https://github.com/plesk/desktop</ExternalLink><br/>
+          Wiki: <ExternalLink href="https://github.com/plesk/desktop/wiki">https://github.com/plesk/desktop/wiki</ExternalLink><br/>
+          Ticket: <ExternalLink href="https://github.com/plesk/desktop/issues/new">https://github.com/plesk/desktop/issues/new</ExternalLink><br/>
         </p>
       </div>
     );
