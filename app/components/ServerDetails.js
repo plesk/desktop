5a1ca03c5952ce5397fa212983ba525fae36ff6e
Alexei Yuzhakov <sibprogrammer@gmail.com>
2017-03-26 10:42:05 +0200

    Add an ability to disconnect the server.

--- app/components/ServerDetails.js
+++ app/components/ServerDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+const storage = window.require('electron-json-storage');
 
 export default class ServerDetails extends React.Component {
   render() {
@@ -6,8 +7,23 @@ export default class ServerDetails extends React.Component {
     return (
       <div>
         <h1 className="page-header">Server {serverName}</h1>
-        TODO: server details
+        <a className="btn btn-default" onClick={this.handleDisconnect.bind(this)}>Disconnect</a>
       </div>
     );
+  }
+
+  handleDisconnect(event) {
+    event.preventDefault();
+
+    const { serverName } = this.props.match.params;
+
+    storage.get('servers', (error, servers) => {
+      delete servers[serverName];
+      storage.set('servers', servers, (error) => {
+        if (error) throw error;
+      });
+    });
+
+    this.props.history.push('/');
   }
 }
