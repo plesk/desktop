6b28f700878fc12e5cfb65eae013dfdc82cf4b71
Alexei Yuzhakov <ayuzhakov@plesk.com>
2017-04-18 10:39:20 +0700

    Fix code formatting.

--- app/api-rpc/Subscription.js
+++ app/api-rpc/Subscription.js
@@ -5,50 +5,50 @@ function Subscription() {
 
   function _getMultiServerRequestSettings() {
     return (
-    `<request-settings>
-      <setting>
-        <name>plesk_rpc_forwarding_to_ext</name>
-        <value>plesk-multi-server</value>
-      </setting>
-      <setting>
-        <name>ext-plesk-multi-server:ipv4</name>
-        <value>shared</value>
-      </setting>
-      <setting>
-        <name>ext-plesk-multi-server:sync</name>
-        <value>true</value>
-      </setting>
-    </request-settings>`
+      `<request-settings>
+        <setting>
+          <name>plesk_rpc_forwarding_to_ext</name>
+          <value>plesk-multi-server</value>
+        </setting>
+        <setting>
+          <name>ext-plesk-multi-server:ipv4</name>
+          <value>shared</value>
+        </setting>
+        <setting>
+          <name>ext-plesk-multi-server:sync</name>
+          <value>true</value>
+        </setting>
+      </request-settings>`
     );
   }
 
   function _getSubscriptionCreationPacket(requestSettings, domain, domainLogin, domainPassword, ipAddress) {
     return (
-    `<packet>
-      ${requestSettings}
-      <webspace>
-        <add>
-          <gen_setup>
-            <name>${domain}</name>
-            <ip_address>${ipAddress}</ip_address>
-          </gen_setup>
-          <hosting>
-            <vrt_hst>
-              <property>
-                <name>ftp_login</name>
-                <value>${domainLogin}</value>
-              </property>
-              <property>
-                <name>ftp_password</name>
-                <value>${domainPassword}</value>
-              </property>
+      `<packet>
+        ${requestSettings}
+        <webspace>
+          <add>
+            <gen_setup>
+              <name>${domain}</name>
               <ip_address>${ipAddress}</ip_address>
-            </vrt_hst>
-          </hosting>
-          <plan-name>Default domain</plan-name>
-        </add>
-      </webspace>
-    </packet>`
+            </gen_setup>
+            <hosting>
+              <vrt_hst>
+                <property>
+                  <name>ftp_login</name>
+                  <value>${domainLogin}</value>
+                </property>
+                <property>
+                  <name>ftp_password</name>
+                  <value>${domainPassword}</value>
+                </property>
+                <ip_address>${ipAddress}</ip_address>
+              </vrt_hst>
+            </hosting>
+            <plan-name>Default domain</plan-name>
+          </add>
+        </webspace>
+      </packet>`
     );
   }
 
