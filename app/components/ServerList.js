c682a8a63290a006d0526fe6b03606854c970eeb
Alexei Yuzhakov <ayuzhakov@plesk.com>
2017-04-18 10:24:44 +0700

    Fix code formatting.

--- app/components/ServerList.js
+++ app/components/ServerList.js
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom';
 
 class ServerList extends React.Component {
   render() {
-  const { servers } = this.context.storage;
+    const { servers } = this.context.storage;
 
-  return (
-    <ul className="nav nav-sidebar">
-      {Object.keys(servers).map((name) => {
-        const serverDetails = servers[name].details || {};
-        return (
-          <li key={name}>
-          <Link to={`/server/show/${name}`}>
-            <span className="glyphicon glyphicon-globe"></span>&nbsp;
-            {name}<br/>
-            <small className="plesk-version">{`${serverDetails.version}, ${serverDetails.os} ${serverDetails.osVersion}`}</small>
-          </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
+    return (
+      <ul className="nav nav-sidebar">
+        {Object.keys(servers).map((name) => {
+          const serverDetails = servers[name].details || {};
+          return (
+            <li key={name}>
+            <Link to={`/server/show/${name}`}>
+              <span className="glyphicon glyphicon-globe"></span>&nbsp;
+              {name}<br/>
+              <small className="plesk-version">{`${serverDetails.version}, ${serverDetails.os} ${serverDetails.osVersion}`}</small>
+            </Link>
+            </li>
+          );
+        })}
+      </ul>
+    );
   }
 }
 
